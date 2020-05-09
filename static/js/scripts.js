f33ea83b17eb673c5dab9baee03e9f68689a62c1
Teck Mee_May <kongteckmee@gmail.com>
2020-05-09 05:37:59 +0000

    Materialize carousel added

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,12 @@
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-    $('.sidenav').sidenav();
+$(document).ready(function () {
+  $(".collapsible").collapsible();
+  $(".sidenav").sidenav();
+  $(".carousel.carousel-slider").carousel({
+    fullWidth: true,
+    indicators: true,
+    dist: 0,
+  });
+  window.setInterval(function () {
+    $(".carousel").carousel("next");
+  }, 5000);
 });
