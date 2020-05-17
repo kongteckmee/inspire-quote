4fce6b20831afdf014b3bb385d917a03f9535a6e
Teck Mee_May <kongteckmee@gmail.com>
2020-05-17 05:22:58 +0000

    Connect to MongoDB

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+DBS_NAME = "inspire_quote"
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", quotes=mongo.db.quote.find())
 
 
 @app.route('/new_inspire')
