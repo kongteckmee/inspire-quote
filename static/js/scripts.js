87327b7770e6326db8fd85c4aba15db4dcfac13a
Teck Mee_May <kongteckmee@gmail.com>
2020-05-30 05:11:09 +0000

    Codes are indented

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,22 +1,20 @@
 $(document).ready(function () {
+  // Materialize Collapsible Initialization
+  $(".collapsible").collapsible();
 
-    // Materialize Collapsible Initialization
-    $(".collapsible").collapsible();
+  // Materialize Sidenav Initialization
+  $(".sidenav").sidenav();
 
-    // Materialize Sidenav Initialization
-    $(".sidenav").sidenav();
+  // Materialize Select Initialization
+  $("select").formSelect();
 
-    // Materialize Select Initialization
-    $("select").formSelect();
-
-    // Materialize Carousel Initialization
-    $(".carousel.carousel-slider").carousel({
-        fullWidth: true,
-        indicators: true,
-        dist: 0
-    });
-    window.setInterval(function () {
-        $(".carousel").carousel("next");
-    }, 5500);
-
+  // Materialize Carousel Initialization
+  $(".carousel.carousel-slider").carousel({
+    fullWidth: true,
+    indicators: true,
+    dist: 0,
+  });
+  window.setInterval(function () {
+    $(".carousel").carousel("next");
+  }, 5500);
 });
